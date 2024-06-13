10252f31b862187fd895ed121d0719ef2378bc7e
Hepheoluwah <mtoyin27@gmail.com>
2024-06-13 14:58:52 +0000

    Web stack debugging

--- 0x1B-web_stack_debugging_4/0-strace_is_your_friend.pp
+++ 0x1B-web_stack_debugging_4/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# fix our stack so that there is not failed requests
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
 }
